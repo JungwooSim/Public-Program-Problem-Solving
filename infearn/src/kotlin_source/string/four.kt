e3b4151bd0f6706a434bb0741a9750e6eb7cfe4f
스냅팀/심정우 <big.sim@musinsa.com>
2022-06-26 14:43:12 +0900

    add : 4. 단어 뒤집기

---
+++ infearn/src/kotlin_source/string/four.kt
@@ -0,0 +1,39 @@
+package kotlin_source.string
+
+import java.lang.System.`in`
+import java.util.*
+
+/*
+4. 단어 뒤집기
+설명
+N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+
+입력
+첫 줄에 자연수 N(3<=N<=20)이 주어집니다.
+두 번째 줄부터 N개의 단어가 각 줄에 하나씩 주어집니다. 단어는 영어 알파벳으로만 구성되어 있습니다.
+
+출력
+N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력합니다.
+
+Ex.
+입력 : good
+출력 : doog
+ */
+fun main() {
+    val scanner = Scanner(`in`)
+
+//    val str: String = scanner.next()
+    val str: String = "good"
+
+    println(solution(str))
+}
+
+fun solution(str: String): String {
+    var result = StringBuilder()
+
+    for (i in str.length downTo 1) {
+        result.append(str[i-1])
+    }
+
+    return result.toString()
+}
